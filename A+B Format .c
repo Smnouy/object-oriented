a979fd0c4fe1d187b1238ab0e1114cb2d991c9fd
deepYY <596903325@qq.com>
2016-01-27 23:06:36 +0800

    Signed-off-by: deepYY <596903325@qq.com>

---
+++ A+B Format .c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{	
+	int a, b, c;
+	scanf("%d %d",&a,&b);
+	c = a + b;	
+	if (c < 0){		
+	printf("-");
+	c = -c;	}	
+	if (c >= 1000000)	{printf("%d,%03d,%03d", c / 1000000, (c / 1000) % 1000, c % 1000);}		
+	else if (c >= 1000)     {printf("%d,%03d", c / 1000, c % 1000);		}	
+	else    {printf("%d", c);}	
+	return 0;
+}
