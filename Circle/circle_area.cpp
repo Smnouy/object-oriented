bc1f526fa4a8010857a42f5c56b0055eb3af47d8
deepYY <596903325@qq.com>
2016-05-06 09:29:43 +0800

    Added files via upload

---
+++ Circle/circle_area.cpp
@@ -0,0 +1,7 @@
+#include "circle_area.h"
+#include<stdlib.h>
+#include <iostream>
+double Circle_area::get_area(double r)
+{
+	return (r*r*pi);
+}
---
+++ Circle/circle_area.h
@@ -0,0 +1,12 @@
+#ifndef CIRCLE_AREA_H
+#define CIRCLE_AREA_H
+#define pi 3.14
+#include <iostream>
+
+class Circle_area
+{
+	public:
+	double get_area(double r);
+};
+
+#endif
